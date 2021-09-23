4983482a7979d6036b9153c6c17c7d05b11eb82a
Pravin Pawar <pravinvpawar5551@gmail.com>
2021-09-23 14:13:49 +0530

    final commit

--- mealapp/lib/screens/category_meals_screen.dart
+++ mealapp/lib/screens/category_meals_screen.dart
@@ -30,7 +30,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
     if (!_loadedInitData) {
       final routeArgs =
           ModalRoute.of(context).settings.arguments as Map<String, String>;
-      categoryTitle = routeArgs['title'];
+      categoryTitle = routeArgs['title'].toString();
       final categoryId = routeArgs['id'];
       displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
